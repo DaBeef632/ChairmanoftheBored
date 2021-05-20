645022bc2fdeb6b3510e3f2ca629aee75697b679
Greg Conti <53584920+DaBeef632@users.noreply.github.com>
2021-05-20 16:49:16 -0400

    Add files via upload

--- Chairman of the Bored/script/main.js
+++ Chairman of the Bored/script/main.js
@@ -9,9 +9,14 @@ form.addEventListener('submit', (e) => {
 			input.parentElement.classList.add('error');
 		} else {
 			input.parentElement.classList.remove('error');
+      pageRedirect();
+      alert("You have been signed up");
+
 			if (input.type == 'email') {
 				if (validateEmail(input.value)) {
 					input.parentElement.classList.remove('error');
+              pageRedirect();
+      alert("You have been signed up");
 				} else {
 					input.parentElement.classList.add('error');
 				}
@@ -20,7 +25,14 @@ form.addEventListener('submit', (e) => {
 	});
 });
 
+
 function validateEmail (email) {
 	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	return re.test(String(email).toLowerCase());
 }
+
+function pageRedirect(){
+  window.location.replace("index.html");
+
+}
+
