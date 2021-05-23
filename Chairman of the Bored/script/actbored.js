0772e8b300ac58bab4d3075c971eb391deb57116
Greg Conti <53584920+DaBeef632@users.noreply.github.com>
2021-05-23 13:02:45 -0400

    Final fixes, got actbored to work
    actbored works, styled to actbored and bored by numbers to match the index

--- Chairman of the Bored/script/actbored.js
+++ Chairman of the Bored/script/actbored.js
@@ -1,29 +1,56 @@
-window.onload = function () {
-  document.getElementById('btnGetActive').addEventListener('click', getActivity, false)
+const endpoint = `https://www.boredapi.com/api/activity`;
+const button = document.querySelector('.button');
+const form = document.querySelector('#form');
+const activityWrapper = document.querySelector('.activity');
+const typeWrapper = document.querySelector('.type');
+const buttonAfter = document.getElementById("#buttonAfter");
+document.querySelector('#buttonAfter').style.display ="none";
+document.querySelector('.button').addEventListener('click', showBtn)
+function showBtn() {
+  document.querySelector('#buttonAfter').style.display = "visible";
+
+
 }
 
-var xmlhttp = new XMLHttpRequest();
-
-
-function getActivity()
-{
- var url = `https://www.boredapi.com/api/activity`
- xmlhttp = new XMLHttpRequest();
- xmlhttp.onreadystatechange = displayActivity();
- xmlhttp.open("GET", url, true);
- xmlhttp.send();
-}
-
-function displayActivity(){
-  if(xmlhttp.readyState == 4 && xmlhttp.status == 200)
-  {
-   console.log(xmlhttp.responseText);
-   var response = xmlhttp.responseText;
-   response.JSON.parse(response);
-   console.log(response.activity);
-   document.getElementById("activity").innerHTML ="<h1>" + response.activity + "</h1>"
-
+const changeColor = (type) => {
+  switch(type){
+    case 'relaxation':
+      return 'MEDIUMSLATEBLUE';
+      break;
+    case 'cooking':
+      return 'KHAKI';
+      break;
+    case 'recreational':
+      return `linear-gradient(to right top, #051937, #004d7a, #008793, #00bf72, #a8eb12)`;
+      break;
+    case 'social':
+      return '#3b5998';
+      break;
+    case 'music':
+      return 'DARKSEAGREEN';
+      break;
+    case 'busywork':
+      return 'CORNFLOWERBLUE';
+      break;
+    default:
+      return 'blue';
   }
-  console.log(displayActivity);
 }
+
+
+const getActivity = async (event) => {
+ event.preventDefault();
+  const response = await fetch(endpoint);
+  const json = await response.json();
+  const {activity, type} = json;
+
+
+  activityWrapper.innerHTML= activity;
+const Color = changeColor(type);
+  button.style.background = Color;
+
+
+}
+form.addEventListener("click", getActivity);
+
 
